a9c59ef483543845c84dd7562b4126a73e7396e6
Cole <cole.n.maxwell99@gmail.com>
2021-07-05 21:37:56 -0500

    Added element selector variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const weatherObject = {
-    value : Number
-    unit: "unit"
-
-}
+//Select elements
+const iconElement = document.querySelector(".weather-icon")
+const temperatueElement = document.querySelector(".temperature-value")
+const descriptionElement = document.querySelector(".temperature-description")
+const locationElement = document.querySelector(".location")
+const notificationElement = document.querySelector(".notification")
