6de91355c8ea56a4b9f2c40f4c85e4733ebf2e88
Cole <cole.n.maxwell99@gmail.com>
2021-07-06 18:22:35 -0500

    Added start of openWeatherMap API

--- app.js
+++ app.js
@@ -15,4 +15,40 @@ weather.temperatue = {
 
 //Additonal variables
 const unitKelvin = 273;
-const apiKep = null; // Note: This will be a "burner" key
+const apiKey = null; // Note: This will be a "burner" key
+
+//Check browser for geolocation support
+if (Geolocation in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>Browser doesn't Support GeoLocation</p>`
+}
+
+//Set user's position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+//Get weather data
+function getWeather(latitude, longitude){
+    let openWeatherMapAPI = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+
+    fetch(openWeatherMapAPI)
+    .then(function(response){
+        let weatherData = response.json();
+        return weatherData
+    })
+    .then(function())
+}
+
+//Display an error for issues getting users geo location
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`
+}
+
+//Convert temperatures
