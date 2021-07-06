622e9ce468c52d8d557f7aff8e5695f579a8bc72
Cole <cole.n.maxwell99@gmail.com>
2021-07-05 21:43:51 -0500

    Added weather object and variables

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 //Select elements
-const iconElement = document.querySelector(".weather-icon")
-const temperatueElement = document.querySelector(".temperature-value")
-const descriptionElement = document.querySelector(".temperature-description")
-const locationElement = document.querySelector(".location")
-const notificationElement = document.querySelector(".notification")
+const iconElement = document.querySelector(".weather-icon");
+const temperatueElement = document.querySelector(".temperature-value");
+const descriptionElement = document.querySelector(".temperature-description");
+const locationElement = document.querySelector(".location");
+const notificationElement = document.querySelector(".notification");
+
+//Weather object
+const weather = {};
+
+//Added object data\
+weather.temperatue = {
+    unit : "celsius"
+}
+
+//Additonal variables
+const unitKelvin = 273;
+const apiKep = null; // Note: This will be a "burner" key
